02f49da281120ca712275c5c6b54c2864882d916
himanshu2285 <himanshu2k05m@gmail.com>
2025-09-13 14:12:30 +0530

    Init

---
+++ ecommerce/ecommerceapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import index
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('/contact', index, name='contact'),
+    path('/about', index, name='about'),
+]

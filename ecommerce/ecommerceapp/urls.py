2b512d278232abd7f2a991530caccc6ce29e45a1
himanshu2285 <himanshu2k05m@gmail.com>
2025-09-14 10:43:32 +0530

    Implemented html template and and static files, also set STATICFILES_DIR

--- ecommerce/ecommerceapp/urls.py
+++ ecommerce/ecommerceapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, contact, about
 
 urlpatterns = [
     path('', index, name='index'),
-    path('/contact', index, name='contact'),
-    path('/about', index, name='about'),
+    path('contact', contact, name='contact'),
+    path('about', about, name='about'),
 ]

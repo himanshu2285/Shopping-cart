5d4caa87d559153564201b3e8c0fda45c37db43c
himanshu2285 <himanshu2k05m@gmail.com>
2025-09-18 14:48:22 +0530

    Added Contact Form

--- ecommerce/ecommerceapp/views.py
+++ ecommerce/ecommerceapp/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from django.contrib import messages 
+from ecommerceapp.models import Contact
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name=request.POST.get("name")
+        email=request.POST.get("email")
+        desc=request.POST.get("desc")
+        pnumber=request.POST.get("pnumber")
+        
+        myquery=Contact(name=name,email=email,desc=desc,phonenumber=pnumber)
+        myquery.save()
+        messages.info(request,"We will get back to you soon..")
+        return render(request,"contact.html")
+        
     return render(request, 'contact.html')
 
 def about(request):

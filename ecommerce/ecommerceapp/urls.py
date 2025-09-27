59be0176367322c5a8ffce3107ee0e18192e90d7
himanshu2285 <himanshu2k05m@gmail.com>
2025-09-27 14:46:19 +0530

    Integrated Paytm Functionality Order Placement

--- ecommerce/ecommerceapp/urls.py
+++ ecommerce/ecommerceapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index, contact, about
+from .views import index, contact, about, checkout, handlerequest
 
 urlpatterns = [
     path('', index, name='index'),
     path('contact', contact, name='contact'),
     path('about', about, name='about'),
+    path('checkout/', checkout, name='Checkout'),
+    path('handlerequest/',handlerequest, name="HandleRequest")
 ]
